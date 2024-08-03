81205c3a66d6ec2fcd34b2de58a73fd7256fca94
Qajay0832 <qajay0832@gmail.com>
2024-08-03 18:26:00 +0530

    second commit

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -3,9 +3,7 @@ import React, { useState } from 'react'
 import '../styles/Child.css'
 
 const Selection = (props) => {
-    console.log(props.applyColor);
     const [colorSetter, setcolorSetter] = useState("");
-    console.log(colorSetter);
   return (
     <div className='fix-box' style={{backgroundColor:colorSetter}} onClick={() =>{setcolorSetter(props.data.background)}} >
         Section {props.index+1}
